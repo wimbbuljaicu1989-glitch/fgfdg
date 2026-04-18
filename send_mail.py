497ad30032dbe07bbd04a8efa9b4df8ae646a41f
wimbbuljaicu1989-glitch <wimbbuljaicu1989@gmail.com>
2026-04-18 12:08:07 +0530

    Add files via upload

---
+++ send_mail.py
@@ -0,0 +1,19 @@
+import smtplib
+import os
+
+email_user = os.environ.get("EMAIL_USER")
+email_pass = os.environ.get("EMAIL_PASS")
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(email_user, email_pass)
+
+subject = "GitHub Action Email"
+body = "This email was sent automatically from GitHub Actions."
+
+message = f"Subject: {subject}\n\n{body}"
+
+server.sendmail(email_user, "speednet.cok@gmail.com", message)
+server.quit()
+
+print("Email sent successfully")

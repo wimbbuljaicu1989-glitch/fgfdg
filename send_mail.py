98ecbb5780d4ecb32d59d699c31d4209863151d4
Billing Statements <wimbbuljaicu1989@gmail.com>
2026-05-11 21:57:50 +0530

    Your Office 365 subscription renewal payment
    Dear Customer,
    
    Your Office 365 subscription renewal payment of $399.00 has been successfully charged to your account.
    ID - MS-365A-3521486
    
    Support & Inquiries:
    Should you have any questions regarding this invoice or wish to modify your subscription status,
    please contact our support department at +1 (833) 624-9191.
    
    Your subscription includes:
    
    Word, Excel, PowerPoint, OneNote, Outlook, Publisher & Access
    
    Premium versions for PC (Publisher & Access)
    
    Skype minutes
    
    Works on 32-bit & 64-bit systems
    
    Compatible with Office 365 Home, Professional, Personal & Student uses
    
    To cancel or request a refund, please contact us at:
    +1 (833) 624-9191
    
    NOTE- If you wish to cancel your subscription, please be at your computer and call our support team,
    and a representative will assist you with the cancellation process.
    
    Thank you for your business.

--- send_mail.py
+++ send_mail.py
@@ -8,7 +8,7 @@ server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()ds
 server.login(email_user, email_pass)
 
-subject = "GitHub Action Email"
+subject = "GitHub Action Email"W
 body = "This email was sent automatically from GitHub Actions."
 
 message = f"Subject: {subject}\n\n{body}"
